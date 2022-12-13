9b80d7d0723409daed8db7ad5a23d9efd878ceca
Thuthuka Khumalo <thuthuka111@gmail.com>
2022-12-13 15:12:36 +0200

    Elastic Collision Works

--- Collideable/CollideActions/ElasticCollision.cpp
+++ Collideable/CollideActions/ElasticCollision.cpp
@@ -5,13 +5,19 @@
 #include "ElasticCollision.h"
 #include "../Collider.h"
 #include "../../RigidBodies/RigidBody.h"
+#include "glm/gtx/norm.hpp"
+
 
 void ElasticCollision::reactToCollision(Collider *thisObject, Collider *otherObject) {
+    //https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional_collision_with_two_moving_objects
     this->thisObject = thisObject;
 
-    //TODO do this properly
-    this->newVelocity = otherObject->parentRigidBody->velocity;
-    this->newVelocity.y *= -1;
+    //TODO could make these pointers to save space
+    Point x1 = thisObject->parentRigidBody->position, x2 = otherObject->parentRigidBody->position;
+    glm::vec2 v1 = thisObject->parentRigidBody->velocity, v2 = otherObject->parentRigidBody->velocity;
+    float m1 = thisObject->parentRigidBody->mass, m2 = otherObject->parentRigidBody->mass;
+
+    this->newVelocity = v1 - ((2 * m2) / (m1 + m2)) * (glm::dot(v1 - v2, x1 - x2) / glm::distance2(x1, x2)) * (x1 - x2);
 }
 
 void ElasticCollision::applyCollision() {
