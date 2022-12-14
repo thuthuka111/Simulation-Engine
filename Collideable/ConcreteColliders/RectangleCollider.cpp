c846a13d4b90ac4004f82e34fed444486d7e7e6a
Thuthuka Khumalo <thuthuka111@gmail.com>
2022-12-14 08:24:42 +0200

    rectangle returns correct interval

--- Collideable/ConcreteColliders/RectangleCollider.cpp
+++ Collideable/ConcreteColliders/RectangleCollider.cpp
@@ -10,7 +10,23 @@ enum CollisionType;
 RectangleCollider::RectangleCollider(RigidBody *gameObject, float width, float height) : Collider(gameObject, CollisionType::RECTANGLE), width(width), height(height) {}
 
 Interval RectangleCollider::getXInterval() {
-    return Interval{this->parentRigidBody->position.x - this->width / 2, this->parentRigidBody->position.x + this->width / 2}; //TODO make this take the rotation into account
+    float halfWidth = this->width / 2, halfHeight = this->height / 2;
+    Point* rectangleCenter = &this->parentRigidBody->position;
+    Point p1 (rectangleCenter->x + halfWidth, rectangleCenter->y + halfHeight);
+    Point p2 (rectangleCenter->x - halfWidth, rectangleCenter->y + halfHeight);
+    Point p3 (rectangleCenter->x - halfWidth, rectangleCenter->y - halfHeight);
+    Point p4 (rectangleCenter->x + halfWidth, rectangleCenter->y - halfHeight);
+
+    p1 = Collider::rotatePoint(*rectangleCenter, this->parentRigidBody->rotation, p1);
+    p2 = Collider::rotatePoint(*rectangleCenter, this->parentRigidBody->rotation, p2);
+    p3 = Collider::rotatePoint(*rectangleCenter, this->parentRigidBody->rotation, p3);
+    p4 = Collider::rotatePoint(*rectangleCenter, this->parentRigidBody->rotation, p4);
+
+    using namespace glm;
+    float minX = min(p1.x, min(p2.x, min(p3.x, p4.x)));
+    float maxX = max(p1.x, max(p2.x, max(p3.x, p4.x)));
+
+    return Interval{minX / 2, maxX};
 }
 
 bool RectangleCollider::isIntersecting(RectangleCollider *object2) {
